6f6c76d20f08982372617afc54d24991e414eaea
Remy Sharp <remy@remysharp.com>
2020-05-09 19:52:30 +0100

    tweak build

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const http = require("http");
-const url = require("url");
-const unzip = require("unzip-stream");
-const axios = require("axios");
-const { extname } = require("path");
-const { Sequelize, Model, DataTypes } = require("sequelize");
+const http = require('http');
+const url = require('url');
+const unzip = require('unzip-stream');
+const axios = require('axios');
+const { extname } = require('path');
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const index = require('fs').readFileSync('./index.html');
 
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL || "sqlite:zxdb.sqlite"
+  process.env.DATABASE_URL || 'sqlite:zxdb.sqlite'
 );
 
 http
@@ -14,20 +15,26 @@ http
     const parsed = url.parse(req.url, true);
 
     req.query = parsed.query;
+    req.pathname = parsed.pathname;
 
-    if (parsed.pathname === "/get") {
+    if (parsed.pathname.startsWith('/get/')) {
       getFile(req, res);
     } else {
-      findFile(req, res);
+      if (parsed.query.s) {
+        findFile(req, res);
+      } else {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        res.end(index);
+      }
     }
   })
   .listen(process.env.PORT || 8000);
 
 async function getFile(req, res) {
-  const id = req.query.s;
+  const id = parseInt(req.pathname.split('/').pop(), 10);
 
   const result = await sequelize.query(
-    "select * from downloads d where d.id = ?",
+    'select * from downloads d where d.id = ?',
     {
       replacements: [id],
       type: sequelize.QueryTypes.SELECT,
@@ -42,17 +49,17 @@ async function getFile(req, res) {
 
   const url =
     `https://archive.org/download/World_of_Spectrum_June_2017_Mirror/World%20of%20Spectrum%20June%202017%20Mirror.zip/World%20of%20Spectrum%20June%202017%20Mirror/sinclair/` +
-    file_link.substring(file_link.indexOf("/games") + 1);
+    file_link.substring(file_link.indexOf('/games') + 1);
 
-  const ext = extname(file_link.replace(/.zip$/, "")).toUpperCase();
+  const ext = extname(file_link.replace(/.zip$/, '')).toUpperCase();
 
   const { data } = await axios({
     url,
-    responseType: "stream",
-    method: "get",
+    responseType: 'stream',
+    method: 'get',
   });
 
-  data.pipe(unzip.Parse()).on("entry", (entry) => {
+  data.pipe(unzip.Parse()).on('entry', (entry) => {
     var filePath = entry.path;
     if (filePath.toUpperCase().endsWith(ext)) {
       entry.pipe(res);
@@ -67,9 +74,9 @@ function findFile(req, res) {
 
   sequelize
     .query(
-      "select d.id as download_id, * from entries e, downloads d where e.title like :search_name and e.id == d.entry_id and d.filetype_id in (8, 10)",
+      'select d.id as download_id, * from entries e, downloads d where e.title like :search_name and e.id == d.entry_id and d.filetype_id in (8, 10) limit 10',
       {
-        replacements: { search_name: term + "%" },
+        replacements: { search_name: term + '%' },
         type: sequelize.QueryTypes.SELECT,
       }
     )
@@ -77,11 +84,11 @@ function findFile(req, res) {
       result = result.map(
         ({ download_id, title, file_link, release_year }) =>
           `${download_id}^${title}^${file_link
-            .split("/")
+            .split('/')
             .pop()
-            .replace(/.zip/, "")}^${release_year}`
+            .replace(/.zip/, '')}^${release_year}`
       );
 
-      res.end(result.join("\n") + '\n');
+      res.end(result.join('\n') + '\n');
     });
 }

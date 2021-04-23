f2555c55015e156a5499f267f14a5758349bd49e
Remy Sharp <remy@remysharp.com>
2021-04-23 19:14:15 +0100

    add support for new version API + update db

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 require('renvy'); // load up env
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 const unzip = require('unzip-stream');
 const axios = require('axios');
 const { extname } = require('path');
 const { Sequelize, Model, DataTypes } = require('sequelize');
 const index = require('fs').readFileSync('./index.html');
+
+const alt = fs
+  .readFileSync('./alt-source.txt', 'utf-8')
+  .split('\n')
+  .filter(Boolean)
+  .map((_) => parseInt(_, 10));
 
 const sequelize = new Sequelize(
   process.env.DATABASE_URL || 'sqlite:zxdb.sqlite',
@@ -36,23 +43,53 @@ const machineTypeMap = new Map([
   [27, 'N'],
 ]);
 
+const API = {
+  v1: {
+    find: findFile,
+    get: getFile,
+  },
+  v2: {
+    find: findFileV2,
+    get: getFile,
+  },
+  test: {
+    find: findFileV2,
+    get: getFile,
+  },
+};
+
+API.fallback = API['1'];
+
 http
   .createServer(function (req, res) {
     const parsed = url.parse(req.url, true);
 
     req.query = parsed.query;
-    req.pathname = parsed.pathname;
+    // req.query.encode = true;
 
-    if (parsed.pathname.startsWith('/7get/')) {
-      req.query.encode = true;
-      getFile(req, res);
-    } else if (parsed.pathname.startsWith('/get/')) {
-      getFile(req, res);
-    } else if (parsed.pathname.startsWith('/test')) {
-      findFileBeta(req, res);
+    const paths = parsed.pathname.split('/').filter(Boolean);
+    let version = 'fallback';
+    let api = API.fallback;
+    if (paths[0] && API[paths[0].toLowerCase()]) {
+      version = paths.shift().toLowerCase();
+      if (API[version]) {
+        api = API[version];
+      }
+    }
+
+    // reconstruct the URL
+    req.pathname = paths.join('/');
+
+    let method = paths.shift();
+
+    if (method === 'get') {
+      api.get(req, res);
+    } else if (method) {
+      res.writeHead(404, { 'content-type': 'text/plain' });
+      res.end('404: not found');
     } else {
       if (parsed.query.s) {
-        findFile(req, res);
+        api.find(req, res);
       } else {
         res.writeHead(200, { 'content-type': 'text/html' });
         res.end(index);
@@ -115,9 +152,117 @@ async function getFile(req, res) {
   }
 }
 
-async function findFile(req, res) {
-  const term = req.query.s.replace(/\*/g, ' '); // allow spectrum to space sep with * instead of %22
+async function dbFindFile({ term, page = 0, cat = null }) {
+  term = term.replace(/\*/g, ' ');
+  page = parseInt(page);
 
+  const offset = page * 10;
+
+  return Array.from(
+    await sequelize.query(
+      `SELECT
+          d.id AS download_id,
+          e.id AS entity_id,
+          e.title AS name,
+          *
+      FROM
+        entries e,
+        downloads d
+      WHERE
+        e.title LIKE :search_name
+        AND e.id == d.entry_id
+        AND d.filetype_id in(8, 10)
+        AND ${categoryLookup[cat] || categoryLookup.all}
+        OR
+        e.title LIKE :search_name
+        AND e.id == d.entry_id
+        AND d.filetype_id in (8, 10)
+        AND genretype_id IS NULL
+      LIMIT ${offset},10`,
+      {
+        replacements: {
+          search_name: term + '%',
+        },
+        type: sequelize.QueryTypes.SELECT,
+      }
+    )
+  );
+}
+
+function getParams(req) {
+  const term = req.query.s.replace(/\*/g, ' ');
+  const page = parseInt(req.query.p || '0', 10);
+
+  return {
+    term,
+    page,
+  };
+}
+
+async function getMetadata(results) {
+  return Promise.all(
+    results.map((_) => {
+      console.log({ _ });
+      const path = _.file_link.split('/').slice(3).join('/');
+      const ext = path.split('.').slice(-2, -1)[0].toUpperCase();
+      const zxdb = _.file_link.startsWith('/zxdb/');
+      const host = zxdb ? process.env.ZXDB_HOST : process.env.WOS_HOST;
+      const url = `${host}/${path}_CONTENTS/`;
+
+      if (zxdb) return [];
+
+      return axios
+        .get(url)
+        .then(({ data }) => {
+          return data
+            .sort((a, b) => (a.name < b.name ? -1 : 1))
+            .filter((_) => _.name.toUpperCase().endsWith(ext));
+        })
+        .catch((e) => {
+          return [];
+        });
+    })
+  );
+}
+
+async function findFileV2(req, res) {
+  const { term, page } = getParams(req);
+  const sorted = await dbFindFile({ term, page, cat: req.query.cat });
+  const meta = await getMetadata(sorted);
+
+  const str =
+    sorted
+      .map(
+        (
+          {
+            entity_id,
+            download_id,
+            machinetype_id,
+            title,
+            file_link,
+            release_year,
+          },
+          i
+        ) => {
+          if (!meta[i].length) return null;
+          const machine = machineTypeMap.get(machinetype_id) || '?';
+          return `^${download_id}^${title}^${file_link
+            .split('/')
+            .pop()
+            .replace(/.zip/, '')}^${meta[i][0].size}^${meta[i].length}^${
+            release_year || '?'
+          }^${machine}^`;
+        }
+      )
+      .filter((_) => _ !== null)
+      .slice(0, 10)
+      .join('\n') + '\n';
+
+  res.setHeader('content-length', str.length);
+  res.end(str);
+}
+
+async function findFile(req, res) {
   // let { data: ids } = await axios({
   //   url: 'https://api.zxinfo.dk/v3/search',
   //   headers: { 'user-agent': 'zxdb.remysharp.com' },
@@ -143,42 +288,10 @@ async function findFile(req, res) {
   //   }
   // );
 
-  const result = await sequelize.query(
-    `select d.id as download_id, e.title as name, * from entries e, downloads d where e.title like :search_name and e.id == d.entry_id and d.filetype_id in (8, 10) and ${
-      categoryLookup[req.query.cat] || categoryLookup.all
-    } limit 10`,
-    {
-      replacements: {
-        search_name: term + '%',
-      },
-      type: sequelize.QueryTypes.SELECT,
-    }
-  );
+  const { term } = getParams(req);
+  const sorted = await dbFindFile({ term, cat: req.query.cat });
 
-  const sorted = Array.from(result);
-
-  // .sort((a, b) => {
-  //   return ids.indexOf(a.entry_id) < ids.indexOf(b.entry_id) ? -1 : 1;
-  // });
-
-  const meta = await Promise.all(
-    sorted
-      .map((_) => _.file_link.split('/').slice(3).join('/'))
-      .map((_) => {
-        const ext = _.split('.').slice(-2, -1)[0].toUpperCase();
-        return axios
-          .get(`${process.env.META_HOST}/${_}_CONTENTS/`)
-          .then(({ data }) => {
-            return data
-              .sort((a, b) => (a.name < b.name ? -1 : 1))
-              .filter((_) => _.name.toUpperCase().endsWith(ext));
-          })
-          .catch((e) => {
-            console.log(e);
-            return [];
-          });
-      })
-  );
+  const meta = await getMetadata(sorted);
 
   const str =
     sorted
@@ -191,71 +304,6 @@ async function findFile(req, res) {
           release_year || '?'
         }^`;
       })
-      .filter((_) => _ !== null)
-      .slice(0, 10)
-      .join('\n') + '\n';
-
-  res.setHeader('content-length', str.length);
-  res.end(str);
-}
-
-async function findFileBeta(req, res) {
-  const term = req.query.s.replace(/\*/g, ' ');
-  const page = parseInt(req.query.p || '0', 10);
-
-  const offset = page * 10;
-
-  const result = await sequelize.query(
-    `select d.id as download_id, e.title as name, * from entries e, downloads d where e.title like :search_name and e.id == d.entry_id and d.filetype_id in (8, 10) and ${
-      categoryLookup[req.query.cat] || categoryLookup.all
-    } limit ${offset},10`,
-    {
-      replacements: {
-        search_name: term + '%',
-      },
-      type: sequelize.QueryTypes.SELECT,
-    }
-  );
-
-  const sorted = Array.from(result);
-
-  const meta = await Promise.all(
-    sorted
-      .map((_) => _.file_link.split('/').slice(3).join('/'))
-      .map((_) => {
-        const ext = _.split('.').slice(-2, -1)[0].toUpperCase();
-        console.log(_);
-        return axios
-          .get(`${process.env.META_HOST}/${_}_CONTENTS/`)
-          .then(({ data }) => {
-            return data
-              .sort((a, b) => (a.name < b.name ? -1 : 1))
-              .filter((_) => _.name.toUpperCase().endsWith(ext));
-          })
-          .catch((e) => {
-            console.log(e.message);
-            return [];
-          });
-      })
-  );
-
-  const str =
-    sorted
-      .map(
-        (
-          { download_id, machinetype_id, title, file_link, release_year },
-          i
-        ) => {
-          if (!meta[i].length) return null;
-          const machine = machineTypeMap.get(machinetype_id) || '?';
-          return `^${download_id}^${title}^${file_link
-            .split('/')
-            .pop()
-            .replace(/.zip/, '')}^${meta[i][0].size}^${meta[i].length}^${
-            release_year || '?'
-          }^${machine}^`;
-        }
-      )
       .filter((_) => _ !== null)
       .slice(0, 10)
       .join('\n') + '\n';
